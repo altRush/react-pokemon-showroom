384453c21c15805731647f33eb70d042d60acb1a
altRush <rushing.wp@gmail.com>
2024-06-17 08:12:18 +0700

    feat: Display sprite when search is valid

--- src/features/display-pokemon/components/DisplayPokemon.tsx
+++ src/features/display-pokemon/components/DisplayPokemon.tsx
@@ -1,5 +1,7 @@
 import { connect } from 'react-redux';
 import { searchPokemon, PokemonState } from '../../../stores/searchedPokemon';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const actionCreators = {
 	searchPokemon
@@ -7,9 +9,23 @@ const actionCreators = {
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function DisplayPokemon({ searchPokemon, searchedPokemon }: any) {
+	const [pokemonSprite, setPokemonSprite] = useState('');
+
+	useEffect(() => {
+		(async () => {
+			if (searchedPokemon) {
+				const { data } = await axios.get(
+					`https://pokeapi.co/api/v2/pokemon/${searchedPokemon}`
+				);
+				setPokemonSprite(data.sprites.front_default);
+			}
+		})();
+	}, [searchedPokemon]);
+
 	return (
 		<>
 			<h3>Search Pokemon</h3>
+			<img src={pokemonSprite} alt="" />
 			<h2>{searchedPokemon}</h2>
 			<div className="card">
 				<input
