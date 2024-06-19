3ed6fdd78af55f04dfa886315ac865aafea90ec2
altRush <rushing.wp@gmail.com>
2024-06-20 06:56:48 +0700

    chore: Delete unrelated Count reducer

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { counterSlice } from './counter';
 import { searchedPokemonSlice } from './searchedPokemon';
 import { pokemonShowroomStackSlice } from './pokemonShowroom';
 
 const store = configureStore({
 	reducer: {
-		counter: counterSlice.reducer,
 		searchedPokemon: searchedPokemonSlice.reducer,
 		pokemonShowroomStack: pokemonShowroomStackSlice.reducer
 	}
