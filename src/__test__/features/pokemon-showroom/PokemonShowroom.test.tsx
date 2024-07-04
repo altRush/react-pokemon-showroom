bd66af9de0d016504d7d2ff8efc459edcd99f405
altRush <rushing.wp@gmail.com>
2024-07-04 18:28:56 +0700

    test: PokemonShowroom cover whole component

--- src/__test__/features/pokemon-showroom/PokemonShowroom.test.tsx
+++ src/__test__/features/pokemon-showroom/PokemonShowroom.test.tsx
@@ -1,36 +1,80 @@
-import { render, screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
-import PokemonShowroom from '../../../features/pokemon-showroom/components/PokemonShowroom';
+
 import configureStore from 'redux-mock-store';
+import userEvent from '@testing-library/user-event';
+import PokemonShowroom from '../../../features/pokemon-showroom/components/PokemonShowroom';
+
+import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
+import { thunk } from 'redux-thunk';
+
 import * as initialShowroomPokemons from '../../../data/initialShowroomPokemons.json';
+import * as gen1Pokemons from '../../../data/gen-1-pokemons.json';
+import * as pokemonShowroom from '../../../stores/pokemonShowroom';
+import * as utils from '../../../utils';
 
-const mockStore = configureStore([]);
+const next3PokemonsProfiles = [
+	{ name: 'charmander', url: 'https://pokeapi.co/api/v2/pokemon/4/' },
+	{ name: 'charmeleon', url: 'https://pokeapi.co/api/v2/pokemon/5/' },
+	{ name: 'charizard', url: 'https://pokeapi.co/api/v2/pokemon/6/' }
+];
 
-console.log({ initialShowroomPokemons });
+describe('<PokemonShowroom />', () => {
+	beforeEach(() => {
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		const middlewares = [thunk as any];
+		const mockStore = configureStore(middlewares);
 
-const store = mockStore({
-	pokemonShowroomStack: {
-		pokemonStack: initialShowroomPokemons
-	}
-});
+		const store = mockStore({
+			pokemonShowroomStack: {
+				pokemonStack: initialShowroomPokemons,
+				currentPokemonIndex: 3
+			}
+		});
 
-test('Initial display', async () => {
-	render(
-		<>
-			<Provider store={store}>
-				<MemoryRouter>
-					<PokemonShowroom />
-				</MemoryRouter>
-			</Provider>
-		</>
-	);
+		render(
+			<>
+				<Provider store={store}>
+					<MemoryRouter>
+						<PokemonShowroom pokemonProfiles={gen1Pokemons} />
+					</MemoryRouter>
+				</Provider>
+			</>
+		);
+	});
 
-	// await userEvent.click(screen.getByText('Load Greeting'));
-	// await screen.findByRole('heading');
+	test('Initial display', async () => {
+		expect(screen.getByText('Venusaur')).toBeDefined();
+		expect(screen.getByRole('button')).toHaveTextContent(
+			'Load more pokemons..'
+		);
+		expect(screen.getByText('Back to menu').closest('a')).toHaveAttribute(
+			'href',
+			'/'
+		);
+	});
 
-	expect(screen.getByText('Venusaur')).toHaveTextContent('Venusaur');
-	expect(screen.getByRole('button')).toHaveTextContent('Load more pokemons..');
+	test('Click on Load more pokemons..', async () => {
+		jest
+			.spyOn(utils, 'getMoreThreePokemonsProfiles')
+			.mockReturnValue(next3PokemonsProfiles);
+
+		const updatePokemonIndexSpy = jest.spyOn(
+			pokemonShowroom,
+			'updatePokemonIndex'
+		);
+
+		const fetchThreePokemonProfilesSpy = jest.spyOn(
+			pokemonShowroom,
+			'fetchThreePokemonProfiles'
+		);
+
+		await userEvent.click(screen.getByText('Load more pokemons..'));
+
+		expect(fetchThreePokemonProfilesSpy).toHaveBeenCalledWith(
+			next3PokemonsProfiles
+		);
+		expect(updatePokemonIndexSpy).toHaveBeenCalledWith(6);
+	});
 });
